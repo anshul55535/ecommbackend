7af8d7a0b8f164c40ae4b28abac34c1201fdd539
anshul55535 <anshul3926@gmail.com.com>
2023-06-02 19:56:23 +0530

    20

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const cors = require("cors");
 
 const PORT = process.env.PORT || 3000
 
+const app = express();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+app.use(cors())
+
 
 require('dotenv').config();
 
-const app = express();
-app.use(cors())
 app.use('/uploads', express.static('uploads')); 
 
 
